16f5b35b882c5f525e38adbfdf4c121498927729
Joshua Ling <joshling1919@gmail.com>
2018-08-09 13:15:17 -0700

    Turn errors into snackbar

--- src/components/ErrorList.js
+++ src/components/ErrorList.js
@@ -1,41 +1,9 @@
 import React from "react";
 
-export default class ErrorList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      opened: true,
-    };
-  }
+const ErrorList = ({ errors }) => (
+  <div className="panel panel-danger errors error-snackbar">
+    {errors[0].stack}
+  </div>
+);
 
-  closeModal = () => {
-    this.setState({ opened: false });
-  };
-
-  render() {
-    const { errors } = this.props;
-    const { opened } = this.state;
-    return (
-      <div className={`panel panel-danger errors error-modal-${opened}`}>
-        <div className="error-modal-content">
-          <div className="panel-heading">
-            <h3 className="panel-title">Errors</h3>
-          </div>
-          <ul className="list-group">
-            {errors.map((error, i) => (
-              <li key={i} className="list-group-item text-danger">
-                {error.stack}
-              </li>
-            ))}
-          </ul>
-          <button
-            type="button"
-            className="close-modal-button"
-            onClick={this.closeModal}>
-            Got it!
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
+export default ErrorList;
