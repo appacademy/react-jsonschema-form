8765517fe7b835cfa4db746a3ed3025971b45783
Joshua Ling <joshling1919@gmail.com>
2018-08-09 13:39:17 -0700

    Add disappearing logic for snackbar

--- src/components/ErrorList.js
+++ src/components/ErrorList.js
@@ -1,9 +1,35 @@
 import React from "react";
 
-const ErrorList = ({ errors }) => (
-  <div className="panel panel-danger errors error-snackbar">
-    {errors[0].stack}
-  </div>
-);
+class ErrorList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: true,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ visible: false }), 3000);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors[0]) {
+      this.setState({ visible: true }, () => {
+        setTimeout(() => this.setState({ visible: false }), 3000);
+      });
+    }
+  }
+
+  render() {
+    const { errors } = this.props;
+    const { visible } = this.state;
+
+    return (
+      <div className={`panel panel-danger errors error-snackbar-${visible}`}>
+        {errors[0].stack}
+      </div>
+    );
+  }
+}
 
 export default ErrorList;
