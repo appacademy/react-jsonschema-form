6de5835ff100402d35248b278d6635da05a28bd9
Joshua Ling <joshling1919@gmail.com>
2018-08-09 11:45:24 -0700

    Make errors a modal

--- src/components/ErrorList.js
+++ src/components/ErrorList.js
@@ -4,18 +4,20 @@ export default function ErrorList(props) {
   const { errors } = props;
   return (
     <div className="panel panel-danger errors">
-      <div className="panel-heading">
-        <h3 className="panel-title">Errors</h3>
+      <div>
+        <div className="panel-heading">
+          <h3 className="panel-title">Errors</h3>
+        </div>
+        <ul className="list-group">
+          {errors.map((error, i) => {
+            return (
+              <li key={i} className="list-group-item text-danger">
+                {error.stack}
+              </li>
+            );
+          })}
+        </ul>
       </div>
-      <ul className="list-group">
-        {errors.map((error, i) => {
-          return (
-            <li key={i} className="list-group-item text-danger">
-              {error.stack}
-            </li>
-          );
-        })}
-      </ul>
     </div>
   );
 }
