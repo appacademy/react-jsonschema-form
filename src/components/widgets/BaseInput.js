01181fd7ee027b1656d0725caa674497e4e856db
Joshua Ling <joshling1919@gmail.com>
2018-08-29 17:56:37 -0700

    Add autocomplete

--- src/components/widgets/BaseInput.js
+++ src/components/widgets/BaseInput.js
@@ -1,46 +1,87 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Select from "react-select";
 
-function BaseInput(props) {
-  // Note: since React 15.2.0 we can't forward unknown element attributes, so we
-  // exclude the "options" and "schema" ones here.
-  if (!props.id) {
-    console.log("No id for", props);
-    throw new Error(`no id for props ${JSON.stringify(props)}`);
+class BaseInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+      options: [
+        {
+          value: "test",
+          label: "Test",
+        },
+      ],
+    };
   }
-  const {
-    value,
-    readonly,
-    disabled,
-    autofocus,
-    onBlur,
-    onFocus,
-    options,
-    schema,
-    formContext,
-    registry,
-    ...inputProps
-  } = props;
 
-  inputProps.type = options.inputType || inputProps.type || "text";
-  const _onChange = ({ target: { value } }) =>
-    props.onChange(value === "" ? options.emptyValue : value);
+  onChange = e => {
+    this.setState({ value: e });
+    this.props.onChange(e.value);
+  };
 
-  const { rawErrors, ...cleanProps } = inputProps;
+  onInputChange = e => {
+    this.props.schema.fetchOptions(e).then(data => {
+      console.log(data);
+      this.setState({ options: data });
+    });
+  };
 
-  return (
-    <input
-      className="form-control"
-      readOnly={readonly}
-      disabled={disabled}
-      autoFocus={autofocus}
-      value={value == null ? "" : value}
-      {...cleanProps}
-      onChange={_onChange}
-      onBlur={onBlur && (event => onBlur(inputProps.id, event.target.value))}
-      onFocus={onFocus && (event => onFocus(inputProps.id, event.target.value))}
-    />
-  );
+  render() {
+    const { props } = this;
+
+    // Note: since React 15.2.0 we can't forward unknown element attributes, so we
+    // exclude the "options" and "schema" ones here.
+    if (!props.id) {
+      console.log("No id for", props);
+      throw new Error(`no id for props ${JSON.stringify(props)}`);
+    }
+    const {
+      value,
+      readonly,
+      disabled,
+      autofocus,
+      onBlur,
+      onFocus,
+      options,
+      schema,
+      formContext,
+      registry,
+      ...inputProps
+    } = props;
+
+    inputProps.type = options.inputType || inputProps.type || "text";
+    const _onChange = ({ target: { value } }) =>
+      props.onChange(value === "" ? options.emptyValue : value);
+
+    const { rawErrors, ...cleanProps } = inputProps;
+    if (props.schema.autocomplete) {
+      return (
+        <Select
+          value={this.state.value}
+          onChange={this.onChange}
+          options={this.state.options}
+          onInputChange={this.onInputChange}
+        />
+      );
+    }
+    return (
+      <input
+        className="form-control"
+        readOnly={readonly}
+        disabled={disabled}
+        autoFocus={autofocus}
+        value={value == null ? "" : value}
+        {...cleanProps}
+        onChange={_onChange}
+        onBlur={onBlur && (event => onBlur(inputProps.id, event.target.value))}
+        onFocus={
+          onFocus && (event => onFocus(inputProps.id, event.target.value))
+        }
+      />
+    );
+  }
 }
 
 BaseInput.defaultProps = {
