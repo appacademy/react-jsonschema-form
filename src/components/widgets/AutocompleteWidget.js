ec43f52a3bdf1d4adddb6f6a2db44746187f10f3
Joshua Ling <joshling1919@gmail.com>
2019-10-25 07:55:29 -0700

    Allow custom placeholder

--- src/components/widgets/AutocompleteWidget.js
+++ src/components/widgets/AutocompleteWidget.js
@@ -35,7 +35,7 @@ export default class Autocomplete extends React.Component {
     const { schema } = this.props;
     const autoCompleteProps = {
       cacheOptions: true,
-      placeholder: "Search...",
+      placeholder: schema.creatable || "Search...",
       loadOptions: this.debouncedFetch,
       value: selectedOption,
       onChange: this.onChange,
